a4598b91935bf6fee40232718eb128e7b4dd3061
owtscharenko <owtscharenko@physik.uni-bonn.de>
2017-11-27 18:38:59 +0100

    ENH: added support for multiple tables in one file

--- PyTables2RootTTree.py
+++ PyTables2RootTTree.py
@@ -1,8 +1,10 @@
-"""This script converts a hdf5 table into a CERN ROOT Ttree.
+"""This script converts a hdf5 table into a CERN ROOT Ttree. Tested with ROOT 5.34.36, no support for ROOT 6
 """
 import tables as tb
 import numpy as np
 import ctypes
+
+# from root_numpy import array2root
 from ROOT import TFile, TTree
 from ROOT import gROOT, AddressOf
 
@@ -22,6 +24,7 @@ def get_root_type_descriptor(numpy_type_descriptor):
         'uint16': 's',
         'int8': 'B',
         'uint8': 'b',
+        'float64' : 'f'
     }[str(numpy_type_descriptor)]
 
 
@@ -40,6 +43,7 @@ def get_c_type_descriptor(numpy_type_descriptor):
         'uint16': ctypes.c_ushort,
         'int8': ctypes.c_byte,
         'uint8': ctypes.c_ubyte,
+        'float64' : ctypes.c_longdouble,
     }[str(numpy_type_descriptor)]
 
 
@@ -53,8 +57,9 @@ def init_tree_from_table(table, chunk_size=1):
     table: pytables.table
     chunk_size: int
     '''
+    tree_name = table.name # Assign proper name to Tree
 
-    tree = TTree('Table', 'Converted HDF5 table')
+    tree = TTree(tree_name, 'Converted HDF5 table')
     n_entries = None
     if chunk_size > 1:
         n_entries = ctypes.c_int(chunk_size)
@@ -64,6 +69,7 @@ def init_tree_from_table(table, chunk_size=1):
         tree.Branch(column_name, 'NULL', column_name + '[n_entries]/' + get_root_type_descriptor(table.dtype[column_name]) if chunk_size > 1 else column_name + '/' + get_root_type_descriptor(table.dtype[column_name]))
 
     return tree, n_entries
+
 
 
 def convert_table(input_filename, output_filename, chunk_size = 50000):
@@ -79,31 +85,37 @@ def convert_table(input_filename, output_filename, chunk_size = 50000):
 
     '''
 
-    with tb.open_file(input_filename, 'r') as in_file_h5:
-        hits_table = in_file_h5.root.Hits
+    with tb.open_file(input_filename , 'r') as in_file_h5:
 
+        
         out_file_root = TFile(output_filename, 'RECREATE')
+        for node in in_file_h5.iter_nodes('/','Table'): # loop over all TABLES in input_filename
 
-        tree, chunk_size_tree = init_tree_from_table(hits_table, chunk_size)
+            table = node
+            tree, chunk_size_tree = init_tree_from_table(table, chunk_size)
+    
+            for index in range(0, table.shape[0], chunk_size):
+                
+                hits = table.read(start=index, stop=index + chunk_size)
 
-        for index in range(0, hits_table.shape[0], chunk_size):
-            hits = hits_table.read(start=index, stop=index + chunk_size)
+                column_data = {}  # columns have to be in an additional python data container to prevent the carbage collector from deleting
+    
+                for branch in tree.GetListOfBranches():  # loop over the branches
+                    if branch.GetName() != 'n_entries':
+                        column_data[branch.GetName()] = np.ascontiguousarray(hits[branch.GetName()])  # a copy has to be made to get the correct memory alignement
+                        branch.SetAddress(column_data[branch.GetName()].data)  # get the column data pointer by name and tell the tree its address
+    
+                if index + chunk_size > table.shape[0]:  # decrease tree leave size for the last chunk
+                    chunk_size_tree.value = table.shape[0] - index
+    
+                tree.Fill()
 
-            column_data = {}  # columns have to be in an additional python data container to prevent the carbage collector from deleting
-
-            for branch in tree.GetListOfBranches():  # loop over the branches
-                if branch.GetName() != 'n_entries':
-                    column_data[branch.GetName()] = np.ascontiguousarray(hits[branch.GetName()])  # a copy has to be made to get the correct memory alignement
-                    branch.SetAddress(column_data[branch.GetName()].data)  # get the column data pointer by name and tell the tree its address
-
-            if index + chunk_size > hits_table.shape[0]:  # decrease tree leave size for the last chunk
-                chunk_size_tree.value = hits_table.shape[0] - index
-
-            tree.Fill()
-
-        out_file_root.Write()
+            out_file_root.Write()
         out_file_root.Close()
 
 
 if __name__ == "__main__":
-    convert_table('input.h5', 'output.root', chunk_size = 1)  # chose chunk_size parameter as big as possible to increase speed, but not too big otherwise program runs out of memory
+    
+    input_file = '/media/data/SHiP/SHiP-testbeam-September17/testbeam-analysis/tba_improvements_branch/board_10/set_trigger_delay/module_2/69_module_2_ext_trigger_scan_interpreted'
+    
+    convert_table(input_file + '.h5', input_file + '.root', chunk_size = 50000)  # chose chunk_size parameter as big as possible to increase speed, but not too big otherwise program runs out of memory
